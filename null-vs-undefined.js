b4623264ee446a11a2014ae1ec36d6d9c9e2607d
Dipta Sikder <anotheramit8@gmail.com>
2021-02-03 17:50:42 +0600

    learned about null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,39 @@
+//undefined case-1
+let pakhi;
+console.log(pakhi);
+
+//undefined case-2
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+console.log(add(1,2));
+
+
+//undefined case-3
+function add(num1, num2){
+    console.log(num1 + num2);
+    return 
+}
+console.log(add(1,2));
+
+//undefined case-4
+function add(num1, num2){
+    console.log(num1, num2);
+}
+console.log(add(1));
+
+
+//undefined case-5
+const premik = {name:"smart dude", phone:458421};
+console.log(premik.gf);
+
+//undefined case-6
+let fun = undefined;
+console.log(fun);
+
+//undefined case-7
+let ages = [2, 5, 9]
+console.log(ages[11]);
+
+
+//null can be set as a value
